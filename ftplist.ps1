7ed376a675adccc06620c1c2c03a6b41636098c0
Dimitrios Kyranas <dimitrios@djangsters.de>
2023-05-08 14:36:37 +0200

    Update ftplist.ps1

--- ftplist.ps1
+++ ftplist.ps1
@@ -1,33 +1,56 @@
-# Define the target FTP server URL
-$ftpUrl = "http://your-ftp-server-url.com/"
+param (
+    [string]$Url = "",
+    [string]$LinkText = "",
+    [string]$DownloadPath = ""
+)
 
-# Make the HTTP request and get the content
-$response = Invoke-WebRequest -Uri $ftpUrl
-
-# Parse the response to get the file listings
-$fileListings = $response.Content -split "`n" | Where-Object { $_ -match "(\d{4}-\d{2}-\d{2})" }
-
-# Initialize an empty array to store file information
-$files = @()
-
-foreach ($listing in $fileListings) {
-    $parts = $listing -split "\s+"
-    $dateModified = $parts[0]
-    $timeModified = $parts[1]
-    $filename = $parts[-1]
-
-    # Create a custom PowerShell object to store file information
-    $file = New-Object -TypeName PSObject -Property @{
-        Filename = $filename
-        LastModified = [datetime]::ParseExact("$dateModified $timeModified", "yyyy-MM-dd HH:mm", $null)
-    }
-
-    # Add the file object to the files array
-    $files += $file
+if (-not (Test-Path $DownloadPath)) {
+    New-Item -Path $DownloadPath -ItemType Directory | Out-Null
 }
 
-# Sort the files by the last modified date, and get the most recent one
-$latestFile = $files | Sort-Object LastModified -Descending | Select-Object -First 1
+function Download-File {
+    param (
+        [string]$Url,
+        [string]$Destination
+    )
 
-# Print the filename of the most recently modified file
-Write-Output "The most recently modified file is: $($latestFile.Filename)"
+    $WebClient = New-Object System.Net.WebClient
+    $WebClient.DownloadFile($Url, $Destination)
+}
+
+$WebResponse = Invoke-WebRequest -Uri $Url -UseBasicParsing
+
+$FoundLink = $null
+
+foreach ($Link in $WebResponse.Links) {
+    $LinkHTML = $Link.outerHTML
+    if ($LinkHTML -match '<a[^>]*>(.*?)</a>') {
+        $CurrentLinkText = $matches[1]
+        if ($CurrentLinkText -eq $LinkText) {
+            $FoundLink = $Link
+            break
+        }
+    }
+}
+
+if ($FoundLink -eq $null) {
+    Write-Host "Link with text '$LinkText' not found on the page."
+    exit 1
+}
+
+$FileUrl = $FoundLink.href
+
+$BaseUrl = New-Object System.Uri($Url)
+$AbsoluteUrl = New-Object System.Uri($BaseUrl, $FileUrl)
+
+if ([string]::IsNullOrEmpty($AbsoluteUrl)) {
+    Write-Host "File URL not found for the link with text '$LinkText'."
+    exit 1
+}
+
+$FileName = [System.IO.Path]::GetFileName($AbsoluteUrl.LocalPath)
+$DestinationPath = Join-Path -Path $DownloadPath -ChildPath $FileName
+
+Download-File -Url $AbsoluteUrl -Destination $DestinationPath
+
+Write-Host "File '$FileName' downloaded to '$DownloadPath'."
