5b29db3c2b2b6afb712e337245086a1464c9c84f
Dimitrios Kyranas <dimitrios@djangsters.de>
2023-05-10 13:06:49 +0200

    Update reader.ps1

--- reader.ps1
+++ reader.ps1
@@ -4,22 +4,18 @@ $csvFilePath = "path\to\your\csvfile.csv"
 # Import the contents of the CSV file
 $csvData = Import-Csv -Path $csvFilePath
 
-# Get the number of rows in the CSV file
-$rowCount = $csvData.Count
-
 # Initialize the jagged array
 $jaggedArray = New-Object 'System.Collections.ArrayList'
 
 # Populate the jagged array with the CSV data
-for ($rowIndex = 0; $rowIndex -lt $rowCount; $rowIndex++) {
-    $row = New-Object 'System.Collections.ArrayList'
-    $columns = $csvData[$rowIndex].PSObject.Properties
-
-    foreach ($column in $columns) {
-        $row.Add($column.Value) | Out-Null
+foreach ($row in $csvData) {
+    $rowArray = New-Object 'System.Collections.ArrayList'
+    
+    foreach ($propertyName in $row.PSObject.Properties.Name) {
+        $rowArray.Add($row.$propertyName) | Out-Null
     }
 
-    $jaggedArray.Add($row) | Out-Null
+    $jaggedArray.Add($rowArray) | Out-Null
 }
 
 # Display the contents of the jagged array
