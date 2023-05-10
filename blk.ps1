bc86460f510ea18bf35b3ecdee230f9fd2290f87
Dimitrios Kyranas <dimitrios@djangsters.de>
2023-05-10 11:48:27 +0200

    Update blk.ps1

--- blk.ps1
+++ blk.ps1
@@ -16,3 +16,12 @@ Write-Output $result
 
 # Clean up the job
 Remove-Job -Job $job
+
+# Replace 'installer.exe' with the actual installer file name
+# Replace '/S' with the silent installation switch for your installer, if different
+$installerPath = "C:\path\to\installer.exe"
+$installerArgs = "/S"
+
+# Run the silent installer and wait for it to complete
+Start-Process -FilePath $installerPath -ArgumentList $installerArgs -NoNewWindow -Wait
+
