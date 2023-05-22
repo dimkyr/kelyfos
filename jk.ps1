0da9b2258684b76f0b2fa1b0758cbcb4b3744d34
Dimitrios Kyranas <dimitrios@djangsters.de>
2023-05-22 13:43:00 +0200

    Update jk.ps1

--- jk.ps1
+++ jk.ps1
@@ -1,27 +1,13 @@
-# Get the current working directory
-$currentDirectory = Get-Location
-
+# Start Job
 $job = Start-Job -ScriptBlock {
-    param($dir)
-    
-    # Set the location to the current working directory
-    Set-Location $dir
-
-    # Insert your command here
-    Start-Sleep -Seconds 15   # This is just a placeholder command
-} -ArgumentList $currentDirectory
-
-# Wait for the specified amount of time (in seconds)
-Start-Sleep -Seconds 10
-
-# Check if job is still running
-if ($job.JobStateInfo.State -eq 'Running') {
-    # Stop the job if it is still running
-    Stop-Job -Job $job
-    
-    # Write an error message to the console
-    Write-Error "The job did not complete in the allotted time and was stopped."
+    Start-Sleep -Seconds 5000
 }
 
-# Always clean up the job, whether it completed or was stopped
-Remove-Job -Job $job
+# Wait for job to complete with timeout (in seconds)
+$job | Wait-Job -Timeout 10
+
+# Check to see if any jobs are still running and stop them
+$job | Where-Object {$_.State -ne "Completed"} | ForEach-Object {
+    Stop-Job -Id $_.Id
+    Write-Host "Job with ID $($_.Id) was stopped because it exceeded the timeout."
+}
